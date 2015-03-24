562acb00888fffc8a73c72579c6f9ccb34cc3c41
xsxVeNxsx <koltsovphilip@gmail.com>
2015-03-24 22:39:48 +1000

    Add base_model class. Remove db_connection to model. Add base functions for DB access.

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
+session_start();
 error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
-require_once "controller/base_controller.php";
 require_once "controller/auth_controller.php";
 
-$controllers = array("index" => base_controller,
-                    "auth" => auth_controller);
-
-function db_connect($host, $name, $user, $pass)
-{
-    $db = new PDO('mysql:host='.$host.';dbname='.$name, $user, $pass);
-    $db->query('SET character_set_connection = utf8');
-    $db->query('SET character_set_client = utf8');
-    $db->query('SET character_set_results = utf8');
-    return $db;
-}
+$controllers = ["index" => auth_controller,
+                "auth" => auth_controller];
 
 $controller = $_GET['controller'];
 if (!isset($controller))
     $controller = "index";
-//$db = db_connect('localhost', 'u272679865_admin', 'u272679865_photo', '123123123');
-$db = db_connect('localhost', 'photos', 'admin', '123123123');
 echo (new $controllers[$controller]($db))->execute();
 ?>
---
+++ model/base_model.php
@@ -0,0 +1,44 @@
+<?php
+
+class base_model
+{
+    protected  $table_name, $fields, $db;
+
+    protected function db_connect($host, $name, $user, $pass)
+    {
+        $db = new PDO('mysql:host='.$host.';dbname='.$name, $user, $pass);
+        $db->query('SET character_set_connection = utf8');
+        $db->query('SET character_set_client = utf8');
+        $db->query('SET character_set_results = utf8');
+        return $db;
+    }
+
+    public function __construct($table_name, $fields)
+    {
+        $this->table_name = $table_name;
+        $this->fields = $fields;
+        //$db = db_connect('localhost', 'u272679865_admin', 'u272679865_photo', '123123123');
+        $this->db = $this->db_connect('localhost', 'photos', 'admin', '123123123');
+    }
+
+    public function get($params)
+    {
+        $keys = array_intersect(array_keys($params), $this->fields);
+        $keys = array_map(function ($i) {return "$i = ?";}, $keys);
+        $where = "WHERE ".implode(" AND ", $keys);
+        $q = $this->db->prepare("SELECT * FROM $this->table_name $where");
+        $q->execute(array_values($params));
+        return $q->fetchAll();
+    }
+
+    public function add($params)
+    {
+        $arr = array();
+        foreach ($this->fields as $value)
+            array_push($arr, $params[$value]);
+        $query = "INSERT INTO $this->table_name VALUES('' ".str_repeat(",?", count($this->fields)).")";
+        $this->db->prepare($query)->execute($arr);
+    }
+}
+
+?>
