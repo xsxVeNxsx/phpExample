d1370726320b92e524d9521d6b5cfe63fa886b24
xsxVeNxsx <koltsovphilip@gmail.com>
2015-03-24 22:36:18 +1000

    Remade auth form.

--- view/auth_form.php
+++ view/auth_form.php
@@ -1,6 +1,12 @@
-<form method="POST">
-    <input type="text" name="email">
-    <input type="text" name="password">
-    <input type="submit" name="signUp" value="Sign up">
-    <input type="submit" name="signIn" value="Sign in">
-</form>
+<div id="auth_block">
+    <span id="error">
+    <?php echo $errors?>
+    </span>
+
+    <form action="?controller=auth&action=" method="POST" id="auth_form">
+        <input type="text" name="email" placeholder="E-mail"><br>
+        <input type="text" name="password" placeholder="Password"><br>
+        <input type="submit" name="signUp" id="signUp" value="Sign up">
+        <input type="submit" name="signIn" id="signIn" value="Sign in">
+    </form>
+</div>
