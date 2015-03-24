62bac913ecd3956c923743db418421b4ebe55014
xsxVeNxsx <koltsovphilip@gmail.com>
2015-03-24 22:45:12 +1000

    Add styles and scripts array to base view

--- view/base.php
+++ view/base.php
@@ -2,15 +2,16 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <script type="text/javascript" src="js/jquery.js?"></script>
-        <link type="text/css" rel="stylesheet" href="css/style.css?">
-        <script type="text/javascript" src="js/script.js?"></script>
+        <?php foreach ($scripts as $script)
+            echo "<script type='text/javascript' src='js/$script.js?'></script>";
+          foreach ($styles as $style)
+            echo "<link type='text/css' rel='stylesheet' href='css/$style.css?'>";
+        ?>
         <title><?php echo $title?></title>
     </head>
     <body>
-        <H1>dsada</H1>
         <?php foreach ($blocks as $block)
-            echo $block
+            echo $block;
         ?>
     </body>
 </html>
