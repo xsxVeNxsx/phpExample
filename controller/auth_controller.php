9760248e6e28a07015341559f9e4b60d2ef6bfaa
xsxVeNxsx <koltsovphilip@gmail.com>
2015-03-25 21:23:48 +1000

    Add session handler and modify auth_controller Add signout action Add redirecting

--- controller/auth_controller.php
+++ controller/auth_controller.php
@@ -12,40 +12,43 @@ class auth_controller extends base_controller
         $this->actions = array_merge($this->actions, array("signin", "signup"));
     }
 
-    protected function index()
+    public function index()
     {
-        $message = "";
-        if (!$this->model->is_has_user($_SESSION))
-            $message = "You need to singin";
+        $blocks = [$this->render_block("auth_form")];
+        $this->render($blocks);
+    }
+
+    public function signin()
+    {
+        if ($this->model->is_has_user($_REQUEST))
+        {
+            Sessions::set($this->model->get(["email" => $_REQUEST["email"]])[0]["id"]);
+            header("Location: $this->home_url");
+            return;
+        }
+        $message = "Incorrect email or password";
         $blocks = [$this->render_block("auth_form", ["errors" => $message])];
         $this->render($blocks);
     }
 
-    protected function make_session($user)
+    public function signup()
     {
-        $_SESSION["email"] = $user["email"];
-        $_SESSION["id"] = $user["id"];
-    }
-
-    protected function signin()
-    {
-        $message = "";
-        if (!$this->model->is_has_user($_REQUEST))
-            $message = "Incorrect email or password";
-        else
-            $this->make_session($this->model->get($_REQUEST)[0]);
-        $blocks = [$this->render_block("auth_form", ["errors" => $message])];
-        $this->render($blocks);
-    }
-
-    protected function signup()
-    {
-        $message = "";
-        if ($this->model->is_has_email($_REQUEST))
-            $message = "Email is already in use";
-        else
+        if (!$this->model->is_has_email($_REQUEST))
+        {
             $this->model->add($_REQUEST);
+            Sessions::set($this->model->get(["email" => $_REQUEST["email"]])[0]["id"]);
+            header("Location: $this->home_url");
+            return;
+        }
+        $message = "Email is already in use";
         $blocks = [$this->render_block("auth_form", ["errors" => $message])];
+        $this->render($blocks);
+    }
+
+    public function signout()
+    {
+        Sessions::clear();
+        $blocks = [$this->render_block("auth_form")];
         $this->render($blocks);
     }
 }
--- controller/base_controller.php
+++ controller/base_controller.php
@@ -1,4 +1,5 @@
 <?php
+include_once "session_handler.php";
 
 class base_controller
 {
