3a402c39d92c71f9c71e67010c3a94ee7b8c105a
xsxVeNxsx <koltsovphilip@gmail.com>
2015-03-25 21:35:37 +1000

    Add missed strings at prev commits

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
 error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 require_once "controller/auth_controller.php";
+require_once "controller/profile_controller.php";
 
 $controllers = ["index" => Profile_Controller,
                 "auth" => Auth_Controller,
@@ -10,5 +10,5 @@ $controllers = ["index" => Profile_Controller,
 $controller = $_GET['controller'];
 if (!isset($controller))
     $controller = "index";
-echo (new $controllers[$controller]($db))->execute();
+echo (new $controllers[$controller]())->execute();
 ?>
