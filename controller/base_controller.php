99ff9d3625ae2a1aa441f51a265936f34bed527b
xsxVeNxsx <koltsovphilip@gmail.com>
2015-03-25 21:26:42 +1000

    Add checking of sessions for some requests

--- controller/base_controller.php
+++ controller/base_controller.php
@@ -9,16 +9,25 @@ class Base_Controller
     protected $title = "Main Page";
     protected $scripts = ["jquery"];
     protected $styles = ["style"];
+    protected $need_auth = ["Profile_Controller"];
+    protected $home_url;
 
     public function execute()
     {
+        session_start();
+        $this->home_url = "http://".$_SERVER['HTTP_HOST'].$_SERVER["SCRIPT_NAME"];
         $action = $_REQUEST["action"];
         if (!isset($action) || !in_array($action, $this->actions))
             $action = "index";
+        if (in_array(get_class($this), $this->need_auth) && !Sessions::is_logined())
+        {
+            header("Location: $this->home_url?controller=auth");
+            return;
+        }
         return $this->$action();
     }
 
-    protected function index()
+    public function index()
     {
         $this->render($blocks);
     }
