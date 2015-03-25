d16219c346bb23edf461fb801057301351a81b91
xsxVeNxsx <koltsovphilip@gmail.com>
2015-03-25 21:36:32 +1000

    Add profile page style and photo's loading script

---
+++ js/photos.js
@@ -0,0 +1,28 @@
+function loadPhotos()
+{
+    var url = "http://localhost/farpost?controller=profile&action=get_photos";
+    $.ajax({
+        method: 'GET',
+        url: url,
+        success: function(data){
+            var arr = $("#photo_album_block").children();
+            for (var i = 0; i < arr.length; ++i)
+                $(arr[i]).remove();
+            for (var i = 0; i < data.length; ++i)
+                $("#photo_album_block").append(
+                    $("<div/>", {"class": "photo"}).append(
+                        $("<a/>", {"href": data[i], "target": "_blank"}).append(
+                            $("<img/>", {"src": data[i]})
+                        )
+                    )
+                );
+        }
+    });
+}
+
+function on_document_ready()
+{
+    $("#hidden_frame").load(function() {loadPhotos()});
+}
+
+$(document).ready(on_document_ready);
